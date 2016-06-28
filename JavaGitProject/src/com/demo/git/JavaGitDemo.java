29f9ccd414f31e82691d8ff2c4f28b851685e008
jeetenderbatra <jeetenderbatra@gmail.com>
2016-06-29 01:48:14 +0530

    Adding to fix1 branch

--- JavaGitProject/src/com/demo/git/JavaGitDemo.java
+++ JavaGitProject/src/com/demo/git/JavaGitDemo.java
@@ -4,6 +4,8 @@ public class JavaGitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("This is fix");
+		System.out.println("This is another fix");
 	}
 
 }
